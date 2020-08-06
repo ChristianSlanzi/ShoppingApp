8c8e563c6f4fa339550df3b189fa37dcb32b8aeb
Christian Slanzi <christian.slanzi@gmail.com>
2020-08-06 22:19:24 +0200

    Initial Commit

---
+++ ShoppingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoppingApp
+//
+//  Created by Christian Slanzi on 06.08.20.
+//  Copyright © 2020 Christian Slanzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
