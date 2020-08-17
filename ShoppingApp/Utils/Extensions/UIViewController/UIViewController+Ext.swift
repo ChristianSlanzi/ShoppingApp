e2975275061ccc3600ded88347d0e9a5105e8d56
Christian Slanzi <christian.slanzi@gmail.com>
2020-08-17 16:10:59 +0200

    add missing file from last commit

---
+++ ShoppingApp/Utils/Extensions/UIViewController/UIViewController+Ext.swift
@@ -0,0 +1,36 @@
+//
+//  UIViewController+Ext.swift
+//  Utils
+//
+//  Created by Christian Slanzi on 21.07.20.
+//  Copyright © 2020 Christian Slanzi. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+
+extension UIViewController {
+
+    /// Creates and presents a custom alert on the main thread
+    func presentAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            //let alertVC = AlertViewController(title: title, message: message, buttonTitle: buttonTitle)
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+            alert.addAction(UIAlertAction(title: buttonTitle, style: .default, handler: nil))
+            //alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+            
+            alert.modalPresentationStyle = .overFullScreen
+            alert.modalTransitionStyle = .crossDissolve
+            self.present(alert, animated: true)
+        }
+    }
+
+    /// present SafariViewController
+    func presentSafariViewController(with url: URL) {
+        let safariViewController = SFSafariViewController(url: url)
+        safariViewController.preferredControlTintColor = .systemGreen
+        present(safariViewController, animated: true)
+    }
+
+}
