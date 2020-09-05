33b397d1ce9c240926ad6ea6f38d9f1af229b1bb
Christian Slanzi <christian.slanzi@gmail.com>
2020-09-05 09:30:08 +0200

    split constants in common utils constants and app specific constants

---
+++ ShoppingApp/AppConstants.swift
@@ -0,0 +1,26 @@
+//
+//  AppConstants.swift
+//  ShoppingApp
+//
+//  Created by Christian Slanzi on 05.09.20.
+//  Copyright © 2020 Christian Slanzi. All rights reserved.
+//
+import UIKit
+
+enum Colors {
+    static let darkGreen = UIColor.init(hexFromString: "#006400")
+}
+
+enum SFSymbols {
+    static let location = UIImage(systemName: "mappin.and.ellipse")
+    static let repos = UIImage(systemName: "folder")
+    static let gists = UIImage(systemName: "text.alignleft")
+    static let followers = UIImage(systemName: "heart")
+    static let following = UIImage(systemName: "person.2")
+}
+
+enum Images {
+    static let ghLogo = UIImage(named: "gh-logo")
+    static let placeholder = UIImage(named: "avatar-placeholder")
+    static let emptyStateLogo = UIImage(named: "empty-state-logo")
+}
--- ShoppingApp/Utils/Constants.swift
+++ ShoppingApp/Utils/Constants.swift
@@ -8,24 +8,6 @@
 
 import UIKit
 
-enum Colors {
-    static let darkGreen = UIColor.init(hexFromString: "#006400")  
-}
-
-enum SFSymbols {
-    static let location = UIImage(systemName: "mappin.and.ellipse")
-    static let repos = UIImage(systemName: "folder")
-    static let gists = UIImage(systemName: "text.alignleft")
-    static let followers = UIImage(systemName: "heart")
-    static let following = UIImage(systemName: "person.2")
-}
-
-enum Images {
-    static let ghLogo = UIImage(named: "gh-logo")
-    static let placeholder = UIImage(named: "avatar-placeholder")
-    static let emptyStateLogo = UIImage(named: "empty-state-logo")
-}
-
 enum ScreenSize {
     static let width        = UIScreen.main.bounds.size.width
     static let height       = UIScreen.main.bounds.size.height
