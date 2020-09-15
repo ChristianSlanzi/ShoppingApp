cbb88a7a6088eebd2aa91fec76e5d30bb450b2d5
Christian Slanzi <christian.slanzi@gmail.com>
2020-09-15 23:03:04 +0200

    more functional setters

--- Utils/Functional/FunctionalSetters.swift
+++ Utils/Functional/FunctionalSetters.swift
@@ -35,3 +35,77 @@ public func second<A, B, C>(_ f: @escaping (B) -> C) -> ((A, B)) -> (A, C) {
 //   |> second { $0 + "!" }
 
 // and allows all sort of composition and mutation
+
+// with nested is going to be a bit messy
+// let nested = ((1, true), "Swift")
+
+// nested
+//    |> first { $0 |> second { !$0 } }
+
+// this doesn't compile
+// nested
+//    |> (first >>> second) { !$0 }
+
+// changing the order works
+// nested
+//    |> (second >>> first) { !$0 }
+
+// but looks strange. we need a backward composing opreator
+
+precedencegroup BackwardsComposition {
+    associativity: left
+}
+
+infix operator <<< : BackwardsComposition
+func <<< <A, B, C>(_ f: @escaping (B) -> C, _ g: @escaping (A) -> B) -> (A) -> C {
+    return { f(g($0)) }
+}
+
+// now we can write:
+// nested
+//    |> (first <<< second) { !$0 }
+
+// and compose
+private func incr(_ x: Int) -> Int {
+    return x + 1
+}
+
+private func nestedComposition() {
+    let nested = ((1, true), "Swift")
+    
+    let new = nested
+        |> (first <<< first)(incr)
+        |> (first <<< second) { !$0 }
+        |> second { $0 + "!" }
+    
+    // now we have ((2, false), "Swift!")
+    print(new)
+}
+
+// and even compose transformations in isolation i.e. we create new functions
+
+/*
+private func transformationInIsolation() {
+    let nested = ((1, true), "Swift")
+    
+    let transformation = (first <<< first)(incr)
+    <> (first <<< second) { !$0 }
+    <> second { $0 + "!" }
+    
+    // transformation is a function
+    
+    nested |> transformation
+}
+*/ // but hard to compile.
+
+
+// and even we can combine we our functional map
+
+private func test() {
+    let tupleArray = [(42, ["Swift", "Objective-C"]), (1729, ["Haskell", "Purescript"])]
+    
+    let addEsclamationToAllStringsArray = tupleArray |> (map <<< second <<< map) { $0 + "!"}
+    
+    print(addEsclamationToAllStringsArray)
+}
+
