a887a71e08627d4c3e1d42f1786ec68d4d4d0afe
Christian Slanzi <christian.slanzi@gmail.com>
2020-09-17 23:52:04 +0200

    extend CustomTextField with underlined style

--- ShoppingApp/Custom Views/Text/CustomTextField.swift
+++ ShoppingApp/Custom Views/Text/CustomTextField.swift
@@ -10,13 +10,19 @@ import UIKit
 
 /// An appwide custom TextField utility class
 class CustomTextField: UITextField {
+    
+    enum Style {
+        case none
+        case rounded
+        case underlined
+    }
 
     let padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
     
     var floatingLabel: UILabel = UILabel(frame: CGRect.zero) // Label
     var floatingLabelHeight: CGFloat = 12 // Default height
     
-    var _placeholder: String? // we cannot override 'placeholder'
+    var textPlaceholder: String? // we cannot override 'placeholder'
     
     var floatingLabelColor: UIColor = UIColor.gray {
         didSet {
@@ -27,6 +33,8 @@ class CustomTextField: UITextField {
     
     var activeBorderColor: UIColor = UIColor.lightGray
     
+    var style: Style = .none
+    
     var floatingLabelFont: UIFont = UIFont.systemFont(ofSize: 12) {
         didSet {
             self.floatingLabel.font = self.floatingLabelFont
@@ -35,8 +43,9 @@ class CustomTextField: UITextField {
         }
     }
     // MARK: - Initializers
-    init(placeholder: String) {
-        self._placeholder = placeholder
+    init(placeholder: String, style: Style = .none) {
+        self.textPlaceholder = placeholder
+        self.style = style
         super.init(frame: .zero)
         configure()
     }
@@ -57,32 +66,46 @@ class CustomTextField: UITextField {
     private func configure() {
         translatesAutoresizingMaskIntoConstraints = false
 
-        layer.cornerRadius = 10
-        layer.borderWidth = 2
-        layer.borderColor = UIColor.systemGray4.cgColor
+        if style == .rounded {
+            layer.cornerRadius = 10
+            layer.borderWidth = 2
+            layer.borderColor = UIColor.lightGray.cgColor
+        }
         
-        textColor = .label
-        tintColor = .label // blinking cursor
+        textColor = .gray
+        tintColor = .gray // blinking cursor
         textAlignment = .left
         font = UIFont.preferredFont(forTextStyle: .title2)
         adjustsFontSizeToFitWidth = true
         minimumFontSize = 12
 
-        backgroundColor = .tertiarySystemBackground
+        backgroundColor = .white
         autocorrectionType = .no
         returnKeyType = .go
         clearButtonMode = .whileEditing
         
-        self._placeholder = (self._placeholder != nil) ? self._placeholder : placeholder // Use our custom placeholder if none is set
-        placeholder = self._placeholder // make sure the placeholder is shown
+        self.textPlaceholder = (self.textPlaceholder != nil) ? self.textPlaceholder : placeholder // Use our custom placeholder if none is set
+        placeholder = self.textPlaceholder // make sure the placeholder is shown
         self.floatingLabel = UILabel(frame: CGRect.zero)
+        self.addTarget(self, action: #selector(self.addUnderline), for: .init())
         self.addTarget(self, action: #selector(self.addFloatingLabel), for: .editingDidBegin)
         self.addTarget(self, action: #selector(self.removeFloatingLabel), for: .editingDidEnd)
     }
     
     public func setPlaceholder(_ text: String) {
         placeholder = text
-        self._placeholder = placeholder
+        self.textPlaceholder = placeholder
+    }
+    
+    // Add underline
+    @objc func addUnderline() {
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0.0, y: self.frame.size.height - 1, width: self.frame.size.width, height: 1.0)
+        bottomLine.backgroundColor = UIColor.lightGray.cgColor
+        self.borderStyle = UITextField.BorderStyle.none
+        self.layer.addSublayer(bottomLine)
+        
+        self.setNeedsDisplay()
     }
     
     // Add a floating label to the view on becoming first responder
@@ -90,7 +113,7 @@ class CustomTextField: UITextField {
         if self.text == "" {
             self.floatingLabel.textColor = floatingLabelColor
             self.floatingLabel.font = floatingLabelFont
-            self.floatingLabel.text = self._placeholder
+            self.floatingLabel.text = self.textPlaceholder
             self.floatingLabel.layer.backgroundColor = UIColor.white.cgColor
             self.floatingLabel.translatesAutoresizingMaskIntoConstraints = false
             self.floatingLabel.clipsToBounds = true
@@ -112,7 +135,7 @@ class CustomTextField: UITextField {
                self.subviews.forEach{ $0.removeFromSuperview() }
                self.setNeedsDisplay()
             }
-            self.placeholder = self._placeholder
+            self.placeholder = self.textPlaceholder
         }
         self.layer.borderColor = UIColor.lightGray.cgColor
     }
@@ -127,5 +150,13 @@ class CustomTextField: UITextField {
 
     override open func editingRect(forBounds bounds: CGRect) -> CGRect {
         return bounds.inset(by: padding)
+    }
+    
+    override func layoutSubviews()
+    {
+        super.layoutSubviews()
+        if style == .underlined {
+            addUnderline()
+        }
     }
 }
